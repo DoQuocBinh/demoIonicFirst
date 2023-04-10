19e958794b70cabb7b472c08a9ef7d3207cf155e
DoQuocBinh <binhdq@fpt.edu.vn>
2023-04-10 19:42:20 +0700

    oke

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [name, setName] = useState<string | number>()
+  const [gender, setGender] = useState<string | number>()
+  function registerClick(){
+    alert(`hello ${name}, you are a ${gender}`)
+  }
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>CW Demo</IonTitle>
         </IonToolbar>
+        <IonContent>
+          <IonList>
+            <IonItem>
+              <IonInput placeholder="Enter Name" onIonChange={e=> setName(e.detail.value!)}></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonInput placeholder="Enter Gender" onIonChange={e=> setGender(e.detail.value!)}></IonInput>
+            </IonItem>
+          </IonList>
+          <IonButton onClick={registerClick} expand="block">Register</IonButton>
+        </IonContent>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
     </IonPage>
   );
 };
